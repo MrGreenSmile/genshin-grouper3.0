12ffd096a3b2a90b2cdd8784526fbce1bb94989b
GreenSmile <138634998+MrGreenSmile@users.noreply.github.com>
2023-08-20 14:04:24 +0900

    Add files via upload

--- src/datas/characters_detail.ts
+++ src/datas/characters_detail.ts
@@ -1,7 +1,8 @@
 import diluc_detail from "./diluc";
 import jean_detail from "./jean";
 import mona_detail from "./mona";
+import qiqi_detail from "./qiqi";
 
-const characters_detail = [diluc_detail, jean_detail, mona_detail];
+const characters_detail = [diluc_detail, jean_detail, mona_detail, qiqi_detail];
 
 export default characters_detail;
