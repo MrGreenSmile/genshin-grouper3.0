df3d524920c816f62199f86e9266fe05f3b86280
GreenSmile <138634998+MrGreenSmile@users.noreply.github.com>
2023-08-20 10:15:56 +0000

    Update 2 files

--- src/datas/characters_detail.ts
+++ src/datas/characters_detail.ts
@@ -2,7 +2,14 @@ import diluc_detail from "./diluc";
 import jean_detail from "./jean";
 import mona_detail from "./mona";
 import qiqi_detail from "./qiqi";
+import venti_detail from "./venti";
 
-const characters_detail = [diluc_detail, jean_detail, mona_detail, qiqi_detail];
+const characters_detail = [
+  diluc_detail,
+  jean_detail,
+  mona_detail,
+  qiqi_detail,
+  venti_detail,
+];
 
 export default characters_detail;
